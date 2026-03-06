9cbe35f1b2b47cc89ae5e2ea4eefb03ad07b70ae
kresswell <muiruri0@proton.me>
2026-03-06 21:11:43 +0300

    copied code from official docs

--- src/entry.py
+++ src/entry.py
@@ -1,15 +1,40 @@
-# from workers import Response, WorkerEntrypoint
-# from submodule import get_hello_message
-# class Default(WorkerEntrypoint):
-#     async def fetch(self, request):
-#         return Response(get_hello_message())
+from workers import WorkerEntrypoint
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import asgi
 
-
-from fastapi import FastAPI
-from workers import Response
+class Default(WorkerEntrypoint):
+    async def fetch(self, request):
+        return await asgi.fetch(app, request, self.env)
 
 app = FastAPI()
 
-@app.get("/test")
-async def get_user(user_id: int):
-    return {"message": "Farming is better"}
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+@app.get("/env")
+async def root(req: Request):
+    env = req.scope["env"]
+    return {"message": "Here is an example of getting an environment variable: " + env.MESSAGE}
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
